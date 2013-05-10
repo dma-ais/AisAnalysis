86838b996bd3878cf89f2f174a6261a9e24bc6ca
Ole Borup <obo@frv.dk>
2013-05-10 15:49:02 +0200

    Minor

--- ais-analyzer-viewer/src/main/java/dk/dma/ais/analysis/viewer/AisViewDaemon.java
+++ ais-analyzer-viewer/src/main/java/dk/dma/ais/analysis/viewer/AisViewDaemon.java
@@ -70,7 +70,7 @@ public class AisViewDaemon extends AbstractDaemon {
         Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {            
             @Override
             public void uncaughtException(Thread t, Throwable e) {
-                LOG.error("Uncaught exception in thread " + t.getClass().getCanonicalName() + ": " + e.getMessage(), t);
+                LOG.error("Uncaught exception in thread " + t.getClass().getCanonicalName() + ": " + e.getMessage(), e);
                 System.exit(-1);
             }
         });
